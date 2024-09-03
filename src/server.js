1aea5c4780c6f446eb710ee5c7e81de5b70cdfdd
Ladybert <cyberrashad@gmail.com>
2024-09-03 09:41:42 +0700

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
-const NotesService = require("./services/inMemory/NotesService");
+const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
 const ClientError = require("./exceptions/ClientError");
-
 
 const init = async () => {
   const notesService = new NotesService();
@@ -12,7 +11,7 @@ const init = async () => {
     port: process.env.PORT,
     host: process.env.HOST,
     debug: {
-      request: ['error'],           // tambahkan ini
+      request: ["error"], // tambahkan ini
     },
     routes: {
       cors: {
@@ -29,20 +28,20 @@ const init = async () => {
     },
   });
 
-  server.ext('onPreResponse', (request, h) => {
+  server.ext("onPreResponse", (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
-  
+
     // penanganan client error secara internal.
     if (response instanceof ClientError) {
       const newResponse = h.response({
-        status: 'fail',
+        status: "fail",
         message: response.message,
       });
       newResponse.code(response.statusCode);
       return newResponse;
     }
-      
+
     return h.continue;
   });
 
