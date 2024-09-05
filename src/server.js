1de10d85fe59f9b877d7463152660b5bb66493b2
Ladybert <cyberrashad@gmail.com>
2024-09-05 16:13:36 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 require("dotenv").config();
 const Hapi = require("@hapi/hapi");
+
+// Notes
 const notes = require("./api/notes");
 const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
+
+// Users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
+// ClientError
 const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
+  // Notes
   const notesService = new NotesService();
+
+  // Users
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -20,13 +34,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext("onPreResponse", (request, h) => {
     // mendapatkan konteks response dari request
